24ef0fd1fe49387c0e65248d55fc8cc50125097b
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-16 15:34:28 +0530

    Input_Demo

--- Input_demo.py
+++ Input_demo.py
@@ -2,6 +2,6 @@ name=input("Enter your name: ")
 if (name=="Jaspreet"):
     print("Hey Jaspreet Singh,Welcome Back!")
     print("You are an engineer by profession.")
-    Print("How is your journey of learning python is going on?")
+    print("How is your journey of learning python is going on?")
 else:
     print("Hey You are not Recorded in here")
