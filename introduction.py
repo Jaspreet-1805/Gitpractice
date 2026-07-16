4764322ed3b705f058bfb2cd6fc9ee33b51cffeb
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-16 13:49:57 +0530

    Introduction using Python

---
+++ introduction.py
@@ -0,0 +1,4 @@
+print("Hey Coach,My name is Jaspreet Singh.")
+print("I am an Engineer by profession.")
+print("My Goal is become a great Programmer and Earn 12 Lakh per month.")
+print("Enhance my skills in Python and write my interactive programms to support my family financially.")
