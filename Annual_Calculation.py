f968bdadcaf75c45a1b4ed06d0a820618e1cfeaf
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-16 15:32:49 +0530

    Annual_calculation10/10

--- Annual_Calculation.py
+++ Annual_Calculation.py
@@ -1,5 +1,5 @@
 Monthly_income=70000
 Monthly_savings=20000
-Print("Annual income is",Monthly_income*12)
-Print("Annual savings are",Monthly_savings*12)
+print("Annual income is",Monthly_income*12)
+print("Annual savings are",Monthly_savings*12)
 
---
+++ Input_demo.py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+if (name=="Jaspreet"):
+    print("Hey Jaspreet Singh,Welcome Back!")
+    print("You are an engineer by profession.")
+    Print("How is your journey of learning python is going on?")
+else:
+    print("Hey You are not Recorded in here")
