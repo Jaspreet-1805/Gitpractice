15e910c95593d80b86f9099a2b6eba7b91758692
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-16 14:04:43 +0530

    Intro of Variables

---
+++ Variables_intro.py
@@ -0,0 +1,10 @@
+name="Jaspreet Singh"
+Profession="Engineer"
+current_income="70000"
+target_income="1200000"
+city="Chandigarh"
+print("Hey Coach,My name is",name,".")
+print("I am an",Profession,"by profession.")
+print("My current income is",current_income,"per month.")
+print("My target income is",target_income,"per month.")
+print("with this income I want to buy a house in ",city,".")
