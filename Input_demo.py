f882ac9982501e64d64d85473d207ad685e9ce80
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-16 15:56:08 +0530

    Improvised Input

--- Input_demo.py
+++ Input_demo.py
@@ -1,7 +1,22 @@
-name=input("Enter your name: ")
-if (name=="Jaspreet"):
-    print("Hey Jaspreet Singh,Welcome Back!")
+name = input("Enter your name: ")
+
+if name == "Jaspreet":
+    print("Hey Jaspreet Singh, welcome back!")
     print("You are an engineer by profession.")
-    print("How is your journey of learning python is going on?")
+    print("How is your Python learning journey going?")
+
+elif name == "Gurpal":
+    print("Hey Gurpal, welcome back!")
+    print("You are a teacher by profession.")
+    print("How is your lecturer exam preparation going?")
+
 else:
-    print("Hey You are not Recorded in here")
+    print("Sorry, you are not registered in the system.")
+    register= input("Would you like to register? (yes/no): ")
+    if register == "yes":
+        name = input("Please enter your name: ")
+        profession = input("Please enter your profession: ")
+        print("Thank you for registering", name)
+        print("You are now registered as a", profession)
+    else:
+        print("Thank you for visiting. Have a great day!")
