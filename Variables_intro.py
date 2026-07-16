3e91db04bcc55b5220481330bd596ec8fef809b0
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-16 14:09:27 +0530

    Variable reduced Space 10/10

--- Variables_intro.py
+++ Variables_intro.py
@@ -1,10 +1,10 @@
 name="Jaspreet Singh"
-Profession="Engineer"
+profession="engineer"
 current_income="70000"
 target_income="1200000"
 city="Chandigarh"
 print("Hey Coach,My name is",name,".")
-print("I am an",Profession,"by profession.")
+print("I am an",profession,"by profession.")
 print("My current income is",current_income,"per month.")
 print("My target income is",target_income,"per month.")
-print("with this income I want to buy a house in",city,".")
+print("With this income I want to buy a house in",city,".")
