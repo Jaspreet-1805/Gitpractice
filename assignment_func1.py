95d8034658d14cb75c6c4bb8b84ea6f933edafa4
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-17 12:53:52 +0530

    fuction intro

---
+++ assignment_func1.py
@@ -0,0 +1,8 @@
+def about_me():
+    print("Hello,my name is Jaspreet Singh.")
+    print("I am an engineer by profession.")
+    print("My current income is 70000 per month.")
+    print("My target income is 1200000 per month")
+    print("My dream is to become best algotrader in the world.")
+about_me()
+about_me()
