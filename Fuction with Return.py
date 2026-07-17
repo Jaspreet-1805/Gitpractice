89656f1eb36786271e7ba6f715b60e58f882eef3
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-17 14:23:09 +0530

    Function_advanced

--- Fuction with Return.py
+++ Fuction with Return.py
@@ -1,6 +1,17 @@
-def multiply():
+def input_3num():
     a=int(input("Enter first number: ",))
-    b=int(input("Enter second number: ",))    
-    return a * b
-c=multiply()
-print("The multiplication of two numbers is:",c)
+    b=int(input("Enter second number: ",))
+    c=int(input("Enter third number: ",))
+    return a,b,c
+def compare():
+    x=input_3num({a})
+    y=input_3num({b})
+    z=input_3num({c})
+    if x>y and x>z:
+        return x
+    elif y>x and y>z:
+        return y
+    else:
+        return z
+largest=compare()
+print("The largest number is:",largest)
