22a60cd4d3958602e0b5a157195833978dddeec6
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-17 13:35:57 +0530

    fuction parameters

---
+++ funtion parameters.py
@@ -0,0 +1,8 @@
+def introduce(name, profession, city, dream):
+    print("Name:",name)
+    print("Profession:-",profession)
+    print("City:",city)
+    print("Dream:",dream)
+introduce("Jaspreet Singh","Engineer","Chandigarh","To become best algotrader in the world")
+
+introduce("Gurpal","Teacher","Ropar","To become lecturer")
