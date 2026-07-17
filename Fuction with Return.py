475b346d09e97d3e452c21a30359a0d7534eaf2c
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-17 14:05:22 +0530

    Function_Return_intro

---
+++ Fuction with Return.py
@@ -0,0 +1,6 @@
+def multiply():
+    a=int(input("Enter first number: ",))
+    b=int(input("Enter second number: ",))    
+    return a * b
+c=multiply()
+print("The multiplication of two numbers is:",c)
