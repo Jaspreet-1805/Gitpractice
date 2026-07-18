2daef8ed6a175d6545683898ec6bc788544504d1
Jaspreet Singh <jaspreet2525@hotmail.com>
2026-07-18 15:47:23 +0530

    Square

---
+++ Square(1-5).py
@@ -0,0 +1,2 @@
+for i in range(1,6):
+    print("Square of",i,"is",i*i)
---
+++ total.py
@@ -0,0 +1,6 @@
+total=0
+a=int(input("Enter the starting number :"))
+b=int(input("Enter the end number :"))
+for i in range(a,b+1):
+      total=total+i
+print("The sum of numbers from",a,"to",b,"is",total)
